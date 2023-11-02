e241b8b742a1e16399ea1c88f49cb89091d8d372
Lu Hui <luhui@sipeed.com>
2023-11-02 14:48:35 +0800

    display-setup.sh: fix typo

--- overlay/opt/display-setup.sh
+++ overlay/opt/display-setup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# thank ice
+
 if [ -z "$DISPLAY" ]
 then
 	export DSIPLAY=:0.0
 fi
+
+xinput_dev="pointer:Goodix Capacitive TouchScreen"
 
 if [ ! -d "/sys/class/drm/card0-DSI-1" ]; then
     xrandr --output HDMI-1 --auto --primary
@@ -12,11 +16,11 @@ else
     HDMI_STATUS="$(cat /sys/class/drm/card0-HDMI-A-1/status)"
     if [ "${HDMI_STATUS}" = "disconnected" ]; then
         xrandr --output DSI-1 --auto --rotate right --primary
-        xinput map-to-output 7 DSI-1
+        xinput map-to-output "$xinput_dev" DSI-1
     elif [ "${HDMI_STATUS}" = "connected" ]; then
         xrandr --output HDMI-1 --auto --primary
         xrandr --output DSI-1 --auto --rotate right --below HDMI-1
-        xinput map-to-output 7 DSI-1
+        xinput map-to-output "$xinput_dev" DSI-1
     fi
 fi
 
