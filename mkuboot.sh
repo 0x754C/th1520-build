8b361ada5cfbc23be30e58d7e1a3d0213b1625cd
Lu Hui <luhui@sipeed.com>
2023-11-03 14:39:46 +0800

    add mkuboot.sh

---
+++ mkuboot.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+mkdir -p build
+
+if [ -z "$URL" ]
+then
+	export URL="https://github.com/revyos/thead-u-boot"
+fi
+
+if [ -z "$BRANCH" ]
+then
+	export BRANCH="lpi4a"
+fi
+
+if [ -z "$CROSS_COMPILE" ]
+then
+	export CROSS_COMPILE="riscv64-linux-gnu-"
+fi
+
+set -eux
+
+if [ ! -e build/uboot ] 
+then
+	git clone $URL build/uboot --branch=${BRANCH}
+	cd build/uboot
+	find ../../uboot/ -name *.patch | sort | while read line
+	do
+		patch -p1 < $line
+	done
+	cd ../../
+fi
+
+cd build/uboot
+export ARCH=riscv
+make clean
+make light_lpi4a_defconfig
+make -j$(nproc)
+cp u-boot-with-spl.bin ../u-boot-with-spl-sbc.bin
+make clean
+make light_lpi4a_16g_defconfig
+make -j$(nproc)
+cp u-boot-with-spl.bin ../u-boot-with-spl-sbc-16g.bin
+make clean
+make light_lpi4a_console_defconfig
+make -j$(nproc)
+cp u-boot-with-spl.bin ../u-boot-with-spl-console.bin
