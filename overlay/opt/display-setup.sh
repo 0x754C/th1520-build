9969b85534ecf2b8c64f8f41c67bf730ac3d1aee
Lu Hui <luhui@sipeed.com>
2023-11-08 14:08:12 +0800

    only rotate screen for console

--- overlay/opt/display-setup.sh
+++ overlay/opt/display-setup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 # thank ice
+if (cat /proc/device-tree/model | grep LicheeConsole4A);
+then
+	ROTATE=1
+else
+	ROTATE=0
+fi
 
 if [ -z "$DISPLAY" ]
 then
@@ -15,12 +21,15 @@ if [ ! -d "/sys/class/drm/card0-DSI-1" ]; then
 else
     HDMI_STATUS="$(cat /sys/class/drm/card0-HDMI-A-1/status)"
     if [ "${HDMI_STATUS}" = "disconnected" ]; then
-        xrandr --output DSI-1 --auto --rotate right --primary
-        xinput map-to-output "$xinput_dev" DSI-1
+        if [ "${ROTATE}" = "1" ]; then
+            xrandr --output DSI-1 --auto --rotate right --primary
+            xinput map-to-output "$xinput_dev" DSI-1
+	fi
     elif [ "${HDMI_STATUS}" = "connected" ]; then
         xrandr --output HDMI-1 --auto --primary
-        xrandr --output DSI-1 --auto --rotate right --below HDMI-1
-        xinput map-to-output "$xinput_dev" DSI-1
+        if [ "${ROTATE}" = "1" ]; then
+            xrandr --output DSI-1 --auto --rotate right --below HDMI-1
+            xinput map-to-output "$xinput_dev" DSI-1
+	fi
     fi
 fi
-
