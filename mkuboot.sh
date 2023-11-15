825f8c54e01cb86ea46c240cd45c9fe8ec0cb4b2
Lu Hui <luhui@sipeed.com>
2023-11-15 13:41:39 +0800

    mkuboot.sh: use thead's toolchain

--- mkuboot.sh
+++ mkuboot.sh
@@ -14,7 +14,9 @@ fi
 
 if [ -z "$CROSS_COMPILE" ]
 then
-	export CROSS_COMPILE="riscv64-linux-gnu-"
+	# must use thead's toolchain
+	# https://occ-oss-prod.oss-cn-hangzhou.aliyuncs.com/resource//1663142514282/Xuantie-900-gcc-linux-5.10.4-glibc-x86_64-V2.6.1-20220906.tar.gz
+	export CROSS_COMPILE="riscv64-unknown-linux-gnu-"
 fi
 
 set -eux
