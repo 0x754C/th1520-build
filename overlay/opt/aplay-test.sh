e58b75f4c50741979fdbfd040a1cea5d0357f29e
Lu Hui <luhui@sipeed.com>
2023-11-30 11:39:22 +0800

    overlay: update

---
+++ overlay/opt/aplay-test.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+aplay /usr/share/sounds/alsa/Rear_Center.wav
+aplay /usr/share/sounds/alsa/Rear_Center.wav
+aplay /usr/share/sounds/alsa/Rear_Center.wav
+aplay /usr/share/sounds/alsa/Rear_Center.wav
+aplay /usr/share/sounds/alsa/Rear_Center.wav
+aplay /usr/share/sounds/alsa/Rear_Center.wav
