730c240e501ed7e84795400b8fcbf0144f686aa3
Lu Hui <luhui@sipeed.com>
2023-12-07 14:33:04 +0800

    add test script for z14inch-m0

--- overlay/opt/console-setup.sh
+++ overlay/opt/console-setup.sh
@@ -4,4 +4,4 @@ rm /home/sipeed/.config/autostart/DisplaySetup.desktop
 sudo cp -rv /etc/skel/.config /home/sipeed/
 sudo chown -R sipeed:sipeed /home/sipeed/
 sh /opt/display-setup.sh
-echo 'setup done'
+echo 'console setup done'
